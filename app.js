cc7256424c06e06df4ae142374a8307d115f4924
SameerFaisal <sameerfaisal46@gmail.com>
2023-05-25 14:24:09 +0500

    2212C2 25-05-2023

---
+++ app.js
@@ -0,0 +1,68 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//             $("div").animate({
+//                 left: '250px',
+//                 top:'100px'
+        
+               
+//             });
+//             $("div").animate({
+//             opacity:'0.5'
+        
+               
+//             });
+//             $("div").animate({
+//                 height:'150px',
+//                 width:'500px'
+            
+                   
+//                 });
+          
+  
+//     });
+//   });
+
+// $(document).ready(function(){
+//     $("#gv").click(function(){
+//         let fname=$("#fn").val()
+//         $("#output").html(fname)
+//     })
+// })
+// $(document).ready(function(){
+//     $("#add").click(function(){
+//         let fn=$("#fn").val()
+//         let sn=$("#sn").val()
+//         $("#output").html(parseFloat(fn)+parseFloat(sn))
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("#sub").click(function(){
+//         let fn=$("#fn").val()
+//         let sn=$("#sn").val()
+//         $("#output").html(fn-sn)
+//     })
+// })
+// $(document).ready(function(){
+//     $("#mul").click(function(){
+//         let fn=$("#fn").val()
+//         let sn=$("#sn").val()
+//         $("#output").html(fn*sn)
+//     })
+// })
+// $(document).ready(function(){
+//     $("#div").click(function(){
+//         let fn=$("#fn").val()
+//         let sn=$("#sn").val()
+//         $("#output").html(fn/sn)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+// console.log("Previous SRC: "+$("#tomb").attr("src"))
+// $("#tomb").attr("src","./ai.jpg")
+// console.log("New SRC: "+$("#tomb").attr("src"))
+
+//     })
+// })
